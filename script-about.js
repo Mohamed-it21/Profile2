9c7a3fe3526cca19589964187fd2a28e6ff0f332
Mohamed-it21 <naymwalashino@gmail.com>
2025-07-13 10:14:56 +0200

    Delete script-about.js

--- script-about.js
+++
@@ -1,7 +0,0 @@
-// Trigger fade-up animations when DOM loads
-document.addEventListener("DOMContentLoaded", () => {
-  const fadeElements = document.querySelectorAll(".fade-up");
-  fadeElements.forEach((el) => {
-    el.style.animationPlayState = "running";
-  });
-});
